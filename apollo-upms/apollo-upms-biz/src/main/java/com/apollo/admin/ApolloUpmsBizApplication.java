811503a670202b3972ada78ccbcaf2a1634b4c71
73955 <739557106@qq.com>
2019-04-29 20:13:33 +0800

    upms

---
+++ apollo-upms/apollo-upms-biz/src/main/java/com/apollo/admin/ApolloUpmsBizApplication.java
@@ -0,0 +1,18 @@
+package com.apollo.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 用户统一管理系统
+ *
+ * @author 73955
+ */
+@SpringBootApplication
+public class ApolloUpmsBizApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApolloUpmsBizApplication.class, args);
+    }
+
+}
---
+++ apollo-upms/apollo-upms-biz/src/test/java/com/apollo/admin/ApolloUpmsBizApplicationTests.java
@@ -0,0 +1,16 @@
+package com.apollo.admin;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApolloUpmsBizApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
