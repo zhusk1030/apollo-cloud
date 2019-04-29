d871687e884b94faa5433a4580aef8cc960fbfd6
73955 <739557106@qq.com>
2019-04-29 21:22:56 +0800

    user

---
+++ apollo-upms/apollo-upms-biz/src/main/java/com/apollo/admin/config/MybatisPlusConfigurer.java
@@ -0,0 +1,24 @@
+package com.apollo.admin.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 73955
+ * @date 2019/4/29 20:39
+ */
+@Configuration
+@MapperScan("com.apollo.admin.mapper")
+public class MybatisPlusConfigurer {
+    /**
+     * 分页插件
+     *
+     * @return PaginationInterceptor
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
--- apollo-upms/apollo-upms-biz/src/main/java/com/apollo/admin/controller/UserController.java
+++ apollo-upms/apollo-upms-biz/src/main/java/com/apollo/admin/controller/UserController.java
@@ -3,23 +3,29 @@ package com.apollo.admin.controller;
 import com.apollo.admin.api.entity.SysUser;
 import com.apollo.admin.service.SysUserService;
 import com.apollo.common.core.util.BaseResp;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 /**
+ * 用户控制器
+ *
  * @author 73955
  * @date 2019/4/29 16:45
  */
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @Autowired
     private SysUserService sysUserService;
 
     @PostMapping
     public BaseResp user(@RequestBody SysUser sysUser) {
         return new BaseResp<>(sysUserService.save(sysUser));
+    }
+
+    @GetMapping("/{id}")
+    public BaseResp<SysUser> get(@PathVariable Integer id) {
+        return new BaseResp<>(sysUserService.getById(id));
     }
 
 }
